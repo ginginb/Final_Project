0f3a5f1d6c575dbe4a6988a1f81643970d90aeed
JohnWGray <51131435+JohnWGray@users.noreply.github.com>
2019-12-08 17:22:00 -0600

    Update javaPage.js

--- javaPage.js
+++ javaPage.js
@@ -1,8 +1,7 @@
-	/* JavaScript 6th Edition
+/* JavaScript 6th Edition
    Final Project
    Authors: Ginny Brinkerhoff Fay, John Gray, Hunter Foulkes
    Date: 11/19/2019
-
    Filename: javaPage.js
 */
 var incorrect = "Incorrect!";
@@ -12,7 +11,7 @@ var msg1 = 'Question 1: ';
 var msg2 = 'Question 2: ';
 var msg3 = 'Question 3: ';
 var msg4 = 'Question 4: ';
-var msg5 = 'Question 4: ';
+var msg5 = 'Question 5: ';
 //function is called when Submit is clicked and calls the function for each question
 function send() {
 	send1();
@@ -25,17 +24,22 @@ function send() {
 
 //funtion checks the answers for question 1
 function send1() {
-  var questions = document.getElementsByName("question1");
-  if (questions[0].checked == true) {
-    msg1 += incorrect; return false; 
-  } else if (questions[1].checked == true) {
-    msg1 += correct; return false;
-  } else if (questions[2].checked == true) {
-    msg1 += incorrect; return false;
-  } else {
+  try {
+    var questions = document.getElementsByName("question1");
+  }
+  catch{}
+  finally {
+    if (questions[0].checked == true) {
+      msg1 += incorrect; return false; 
+    } else if (questions[1].checked == true) {
+      msg1 += correct; return false;
+    } else if (questions[2].checked == true) {
+      msg1 += incorrect; return false;
+    } else {
     // no checked
     alert("You must select an answer");
     return false;
+    }
   }
   return true;
 }
@@ -45,17 +49,22 @@ function send1() {
 
 //function checks the answers for question 2
 function send2() {
-  var questions = document.getElementsByName("question2");
-  if (questions[0].checked == true) {
-    msg2 += correct; return false;
-  } else if (questions[1].checked == true) {
-    msg2 += incorrect; return false;
-  } else if (questions[2].checked == true) {
-    msg2 += incorrect; return false;
-  } else {
+  try {
+    var questions = document.getElementsByName("question2");
+  }
+  catch{}
+  finally {
+    if (questions[0].checked == true) {
+      msg2 += correct; return false; 
+    } else if (questions[1].checked == true) {
+      msg2 += incorrect; return false;
+    } else if (questions[2].checked == true) {
+      msg2 += incorrect; return false;
+    } else {
     // no checked
     alert("You must select an answer");
     return false;
+    }
   }
   return true;
 }
@@ -65,17 +74,22 @@ function send2() {
 
 //function checks the answers for question 3
 function send3() {
-  var questions = document.getElementsByName("question3");
-  if (questions[0].checked == true) {
-    msg3 += incorrect; return false;
-  } else if (questions[1].checked == true) {
-    msg3 += incorrect; return false;
-  } else if (questions[2].checked == true) {
-    msg3 += correct; return false;
-  } else {
+  try {
+    var questions = document.getElementsByName("question3");
+  }
+  catch{}
+  finally {
+    if (questions[0].checked == true) {
+      msg3 += incorrect; return false; 
+    } else if (questions[1].checked == true) {
+      msg3 += incorrect; return false;
+    } else if (questions[2].checked == true) {
+      msg3 += correct; return false;
+    } else {
     // no checked
     alert("You must select an answer");
     return false;
+    }
   }
   return true;
 }
@@ -83,17 +97,22 @@ function send3() {
 
 //function checks the answers for question 4
 function send4() {
-  var questions = document.getElementsByName("question4");
-  if (questions[0].checked == true) {
-    msg4 += correct; return false;
-  } else if (questions[1].checked == true) {
-    msg4 += incorrect; return false;
-  } else if (questions[2].checked == true) {
-    msg4 += incorrect; return false;
-  } else {
+  try {
+    var questions = document.getElementsByName("question4");
+  }
+  catch{}
+  finally {
+    if (questions[0].checked == true) {
+      msg4 += correct; return false; 
+    } else if (questions[1].checked == true) {
+      msg4 += incorrect; return false;
+    } else if (questions[2].checked == true) {
+      msg4 += incorrect; return false;
+    } else {
     // no checked
     alert("You must select an answer");
     return false;
+    }
   }
   return true;
 }
@@ -102,17 +121,22 @@ function send4() {
 
 //function checks the answers for question 5
 function send5() {
-  var questions = document.getElementsByName("question5");
-  if (questions[0].checked == true) {
-    msg5 += correct; return false;
-  } else if (questions[1].checked == true) {
-    msg5 += incorrect; return false;
-  } else if (questions[2].checked == true) {
-    msg5 += incorrect; return false;
-  } else {
+  try {
+    var questions = document.getElementsByName("question5");
+  }
+  catch{}
+  finally {
+    if (questions[0].checked == true) {
+      msg5 += correct; return false; 
+    } else if (questions[1].checked == true) {
+      msg5 += incorrect; return false;
+    } else if (questions[2].checked == true) {
+      msg5 += incorrect; return false;
+    } else {
     // no checked
     alert("You must select an answer");
     return false;
+    }
   }
   return true;
 }
@@ -129,5 +153,4 @@ function write() {
 
 //event listener for the submit button to start running all of the functions
 var submitButton = document.getElementById("submit"); 
-submitButton.addEventListener("click", send, false);
-
+submitButton.addEventListener("click", send, false); 
